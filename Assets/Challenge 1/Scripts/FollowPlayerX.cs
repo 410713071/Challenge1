e858461abff631edd241b1733d1ce49a3fadf19d
410713071 <410713071@gms.ndhu.edu.tw>
2020-10-31 17:12:28 +0800

    1031 rigibody and diretion set

--- Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+    private Vector3 offset = new Vector3(0, 0, -10);
 
     // Start is called before the first frame update
     void Start()
