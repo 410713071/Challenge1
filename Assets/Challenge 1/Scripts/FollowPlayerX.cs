8bfe5627287757a89fad4698041995e5e81d9480
410713071 <410713071@gms.ndhu.edu.tw>
2020-11-01 23:27:04 +0800

    AllDirationControllAdded and ThirdPlayermode

--- Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset = new Vector3(0, 0, -10);
+    private Vector3 offset = new Vector3(0, 2, -10);
+    public float distanceUp = 2f;
+    public float distanceAway = 10f;
+    public float distanceback = 6f;
+    public float smooth = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
+        transform.position = plane.transform.position + offset;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = plane.transform.position + offset;
+        
+        Vector3 dispos = plane.transform.position + Vector3.up * distanceUp + Vector3.right * distanceAway + Vector3.back * distanceback ;
+        transform.position = Vector3.Lerp(transform.position, dispos, Time.deltaTime * smooth);
+        transform.LookAt(plane.transform.position);
+       
+
+
     }
 }
--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -7,11 +7,12 @@ public class PlayerControllerX : MonoBehaviour
     public float speed;
     public float rotationSpeed;
     public float verticalInput;
-
+    public float HorizontalInput;
+    public float flipInput;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -19,11 +20,13 @@ public class PlayerControllerX : MonoBehaviour
     {
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
+        HorizontalInput = Input.GetAxis("Horizontal");
+        flipInput = Input.GetAxis("flip");
 
         // move the plane forward at a constant rate
         transform.Translate(Vector3.forward * speed);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+        transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime * verticalInput+ Vector3.up * rotationSpeed * Time.deltaTime * HorizontalInput + Vector3.back * rotationSpeed * 0.7f * Time.deltaTime * flipInput);
     }
 }
